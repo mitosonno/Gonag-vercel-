5522e69c5e6b159a2737df7bdf952e725a0a36b1
mitosonno <nurlan.asgarov@gmail.com>
2026-03-23 16:12:39 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import InvitePage from './InvitePage'
+
+function Root() {
+  const path = window.location.pathname;
+  const inviteMatch = path.match(/^\/invite\/([A-Z0-9]+)$/i);
+  if (inviteMatch) {
+    return React.createElement(InvitePage, {code: inviteMatch[1]});
+  }
+  return React.createElement(App);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(Root))
 )
