65898d314826d8286001f587cf10677047a4f313
mitosonno <nurlan.asgarov@gmail.com>
2026-03-30 00:44:02 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./gonag";
+import App from "./App";
 import InvitePage from "./InvitePage";
 
 createRoot(document.getElementById("root")).render(
