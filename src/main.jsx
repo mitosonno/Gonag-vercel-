a0937de746d704b5c470f454334e9bd1f0e52958
mitosonno <nurlan.asgarov@gmail.com>
2026-03-30 00:20:46 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './gonag'
-import InvitePage from './InvitePage'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./gonag";
+import InvitePage from "./InvitePage";
 
-function Root() {
-  const path = window.location.pathname;
-  const inviteMatch = path.match(/^\/invite\/([A-Z0-9]+)$/i);
-  if (inviteMatch) {
-    return React.createElement(InvitePage, {code: inviteMatch[1]});
-  }
-  return React.createElement(App);
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(Root))
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/invite/:code" element={<InvitePage />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
