5a46d702718fc580013f64b16ebec35856faf80f
mitosonno <nurlan.asgarov@gmail.com>
2026-03-31 01:01:18 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import InvitePage from "./InvitePage";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import InvitePage from './InvitePage'
+import RsvpPage from './RsvpPage'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/invite/:code" element={<InvitePage />} />
-        <Route path="/*" element={<App />} />
+        <Route path="/rsvp/:code" element={<RsvpPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
-);
+)
